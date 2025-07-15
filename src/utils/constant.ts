5d14bb33a5f66f1f6f5b633d7a1debb4b8e000a7
kshitizPratap <kshitiz100mit@gmail.com>
2025-07-15 23:00:46 +0530

    updated url

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -10,7 +10,7 @@ export const API_METHODS = {
 export const tabs = ["basic", "templates", "target"];
 
 // export const API_URL = import.meta.env.VITE_API_URL;
-export const API_URL = "https://revenue-pro-backend-1057029383450.asia-south2.run.app//api/v1";
+export const API_URL = "https://revenue-pro-backend-1057029383450.asia-south2.run.app/api/v1";
 // export const API_URL = " http://localhost:4000/api/v1";
 
 export const targetFields: TargetFieldsConfig = {
