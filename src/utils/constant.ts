306cc4b2c3045377beacb9f6bcc59a2314210dce
Saurabh Nandwal <saurabh21nandwal@gmail.com>
2025-09-15 19:57:56 +0530

    fix be url

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -12,9 +12,8 @@ export const API_METHODS = {
 
 export const tabs = ["basic", "templates", "target"];
 
-// export const API_URL = import.meta.env.VITE_API_URL;
-// export const API_URL = "https://revenue-pro-backend-1057029383450.asia-south2.run.app/api/v1";
-export const API_URL = " http://localhost:4000/api/v1";
+// export const API_URL = " http://localhost:4000/api/v1";
+export const API_URL = "https://revenue-pro-backend-1057029383450.asia-south2.run.app/api/v1";
 
 export const menuItems = [
   { id: 'dashboard', label: 'Dashboard', icon: BarChart3, path: '/' },
